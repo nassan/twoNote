3413a12642a010eb745649b0241e61ff68375770
nassan <nassanpaul@gmail.com>
2013-12-29 23:38:16 +0200

    Added structure to index.js, created getTime methods for getting the current audio time to store with the note objects

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -1,21 +1,78 @@
+// Prepare the audio to be manipulated
+var audio = null;
+// Prepare notes array
+var notesArray = [];
+// Prepare the textarea information
+var textareaData = {
 
-var textareaValue = null;
-var textareaOldValue = null;
-//After 6 seconds setTimeout will check to see what is inside 
-// of the text area 
+	currentValue: null,
+		oldValue: null
+}
+
+function note(actualNotes, timeStamp){
+	this.actualNotes = actualNotes;
+	this.timeStamp = timeStamp;
+	console.log(this.timeStamp)
+}
+
+getCurrentTime = function(){
+	audio = document.getElementById("current-audio");
+	return audio.currentTime;
+}
+	
+	
+textareaData.updateMyValues = function(){
+
+// Get the difference between the current and old textarea values
+
+	this.oldValue = this.currentValue;
+	this.currentValue = $('textarea').val();
+}
+
+
+textareaData.checkMyValueDifferences = function(){
+
+// Returns a boolean indicating whether or not there is a difference
+// between the old and current textarea values
+
+	if (this.oldValue === this.currentValue){
+		return true;
+	};
+	return false;
+}
+
+function sendTextToDiv(){
+
+// Will append the value of the textarea to a div if there is a
+// difference between the last appended text and the current text 
+	
+	textareaData.updateMyValues();
+	changed = textareaData.checkMyValueDifferences();
+	console.log(textareaData.currentValue)
+	if(changed === false){
+		// Then create a new note object
+		newNote = new note(textareaData.currentValue, getCurrentTime())
+		
+		// And push it onto the notesArray for record
+		notesArray.push(newNote)
+
+		// And append it to the div for visualization
+		$("#notes-taken").append('<p class = "note">'+textareaData.currentValue+ newNote.timeStamp+'</p>');	
+	}
+}
+	
 $(document).ready(
 
+
+	
 	setInterval(function(){
-		textareaOldValue = textareaValue;
-		textareaValue = $("textarea").val()
-		console.log(textareaValue);
-		
+		sendTextToDiv()
+	}, 2000)
+	
 
-		if (textareaValue!=textareaOldValue){
-			$("#notes-taken").append('<p>'+textareaValue+'</p>');
-		};
-	},5000)
-
-
+	
+	
 
 );
+
+	// console.log(audio.currentTime())
